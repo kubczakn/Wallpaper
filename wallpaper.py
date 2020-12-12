65357bbfce56117676caa8d8760d50234cd72bf6
kubczakn <kubczakn@umich.edu>
2020-12-11 22:01:21 -0500

    script changes background every hour

--- wallpaper.py
+++ wallpaper.py
@@ -1,7 +1,22 @@
 import ctypes
+import os
+import random
+import sched, time
 
-## Path
 SPI_SETDESKWALLPAPER = 20
 
-def changeWP():
-    ctypes.windll.user32.SystemParametersInfoW(SPI_SETDESKWALLPAPER, 0, "absolute path", 0)
+path = "C:/Users/natha/Documents/Wallpapers/"
+dirs = os.listdir(path)
+
+dirs = [path + file for file in dirs]
+
+s = sched.scheduler(time.time, time.sleep)
+
+
+def wallpaper(sc):
+    ctypes.windll.user32.SystemParametersInfoW(SPI_SETDESKWALLPAPER, 0, random.choice(dirs), 3)
+    s.enter(3600, 1, wallpaper, (s,))
+
+
+s.enter(3600, 1, wallpaper, (s,))
+s.run()
