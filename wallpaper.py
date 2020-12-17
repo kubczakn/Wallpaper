80e111b773b5c23080281165c8655e92f03e5779
kubczakn <kubczakn@umich.edu>
2020-12-16 20:55:34 -0500

    able to download multiple images off a webpage

--- wallpaper.py
+++ wallpaper.py
@@ -1,17 +1,23 @@
 import selenium
+import requests
+import shutil
 import ctypes
 import os
 import random
 from selenium import webdriver
 
-#DRIVER_PATH = "C:/Users/natha/Desktop/Scraping/chromedriver"
-#wd = webdriver.Chrome(executable_path=DRIVER_PATH)
 
-#search_url = "https://www.pexels.com/search/nature%20wallpaper/"
-#wd.get(search_url)
+def download_imgs():
+    DRIVER_PATH = "C:/Users/natha/Desktop/Scraping/chromedriver"
+    wd = webdriver.Chrome(executable_path=DRIVER_PATH)
 
-#thumbnail_results = wd.find_elements_by_id("photo-modal")
-#print(thumbnail_results)
+    search_url = "https://www.pexels.com/search/nature%20wallpaper/"
+    wd.get(search_url)
+
+    images = wd.find_elements_by_class_name("js-download")
+    for image in images:
+        print(image.get_attribute('href'))
+
 
 SPI_SETDESKWALLPAPER = 20
 
@@ -25,4 +31,5 @@ def wallpaper():
     ctypes.windll.user32.SystemParametersInfoW(SPI_SETDESKWALLPAPER, 0, random.choice(dirs), 3)
 
 
+#download_imgs()
 wallpaper()
